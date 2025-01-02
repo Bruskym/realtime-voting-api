620d78711ae380f3cb8cd46603f3639ee51947c6
Antonio Nascimento <ticoanaf@gmail.com>
2025-01-01 22:11:56 -0300

    initial commit

---
+++ src/test/java/com/antonionascimento/voting_api/VotingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonionascimento.voting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
